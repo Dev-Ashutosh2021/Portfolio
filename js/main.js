df2a9400f67dbd94ace1dfb4e884854c0d6d8411
Ashutosh <ashutoshuniyal2101@outlook.com>
2023-08-15 17:38:18 +0530

    Added preloading animation

--- js/main.js
+++ js/main.js
@@ -19,3 +19,14 @@ navBar.forEach(function (a){
     })
 })
 
+//Preloading Animation
+var myVar;
+
+function myFunction() {
+  myVar = setTimeout(showPage, 3000);
+}
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("myDiv").style.display = "block";
+}
